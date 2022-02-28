a1aaa1b1aa7960f6d1046be6c213028c7853b3c3
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-28 13:44:36 +0100

    made componenet expenseItem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
-        <h2>let go</h2>
+      <h2>let go</h2>
+      <p>theis is visible</p>
+      <ExpenseItem />
     </div>
   );
 }
