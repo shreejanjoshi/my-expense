072153b6fb731f93f3e3a6f9bb0930d29de6718b
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-28 15:15:13 +0100

    trying make dynamic data output

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
-    <div className='expense-item'>
-        <div className='expense-item__description'>March 28th 2022</div>
-        <div>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$294.67</div>
-        </div>
+    <div className="expense-item">
+      <div className="expense-item__description">{expenseDate.toISOString()}</div>
+      <div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
     </div>
   );
 }
