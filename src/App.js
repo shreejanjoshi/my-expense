78791b2fc41f3468604fefd269d78385fbbf9444
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-28 16:12:10 +0100

    spliting componenet expense date is now in seprate componenet and also added css

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ function App() {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020,7,14),
+      date: new Date(2020,7,4),
     },
     {
       id: 'e2',
       title: 'Car',
       amount: 194.12,
-      date: new Date(2021,7,14),
+      date: new Date(2021,7,30),
     },
     {
       id: 'e3',
       title: 'TV',
       amount: 92.12,
-      date: new Date(2021,10,14),
+      date: new Date(2021,10,10),
     },
     {
       id: 'e4',
       title: 'House',
       amount: 1094.12,
-      date: new Date(2022,7,14),
+      date: new Date(2022,7,20),
     },
   ]
   return (
