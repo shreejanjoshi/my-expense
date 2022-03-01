7444dd9f13fd9a0b844610721bf81609b0be972d
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-03-01 09:50:06 +0100

    trying to understand state by changing value of title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import React from "react";
+import React, {useState} from "react";
 
 const ExpenseItem = (props) => {
+
+  // is array so 1st value is var oe value itself and 2nd is updating
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updateing');
+  };
+
   return (
     <Card className="expense-item">
       {/* should match the aatribute name date and props.dates.toLocaleString from expreneDate.js */}
       <ExpenseDate date= {props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        {/* change by state */}
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
