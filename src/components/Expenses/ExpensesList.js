862a64414205ccea7f2b5dbd83d096e7ed756c78
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-03-03 15:41:49 +0100

    made a new componenet for list and if no list is found in the show message

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpensesList = (props) => {
+
+  // if no item
+  if(props.items.length === 0){
+      return <h2 className="expenses-list__fallback">Found no expenses</h2>;
+  }
+
+  return (
+      <ul className="expenses-list">
+          {props.items.map((expense) => (
+      //I wanna map every expense into the expenseItem JSX element
+      //we transform our array to an array fill of JSX item
+      <ExpenseItem
+        // to fix key error (66)/  add  props keys to help react identy iedisidure item
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))}
+      </ul>
+  );
+};
+
+export default ExpensesList;
