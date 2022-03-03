6ab8b5ba5b08df29872fe76c4ddc4a8ef3a8144f
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-03-03 11:43:59 +0100

    outputting conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,11 +11,28 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  //items is from app.s map take fun as a argument and fun executes for every element in the array
   // pass function to it if function return true a certain item is kept if not not kepts and filter return brand new array
   // date comes from app.js date
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+  // filter value added expensesContent
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      //I wanna map every expense into the expenseItem JSX element
+      //we transform our array to an array fill of JSX item
+      <ExpenseItem
+        // to fix key error (66)/  add  props keys to help react identy iedisidure item
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div>
@@ -25,18 +42,25 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
 
-        {/* items is from app.s map take fun as a argument and fun executes for every element in the array*/}
-        {/* I wanna map every expense into the expenseItem JSX element */}
-        {/* we transform our array to an array fill of JSX item */}
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            // to fix key error (66)/  add  props keys to help react identy iedisidure item
-            key = {expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
+
+        {/* if output data is empty have to use ternery experssion
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          //I wanna map every expense into the expenseItem JSX element
+          //we transform our array to an array fill of JSX item
+
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              // to fix key error (66)/  add  props keys to help react identy iedisidure item
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
       </Card>
     </div>
   );
