98ba08e645a087faf683283b6d69955c95f13433
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-03-04 08:43:13 +0100

    made functionality for chartbar also css and barfillHeight

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,8 +1,24 @@
 import React from "react";
 import './ChartBar.css';
 
-const ChartBar = () => {
+const ChartBar = (props) => {
 
+    // how much chartbar value should be filled
+    let barFillHeight = '0%';
+
+    if(props.max > 0){
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
+
+    return(
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                {/* style attribute work little diffrently js obj as value */}
+                <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
+            </div>
+            <div className="chart-bar__label">{props.label}</div>
+        </div>
+    );
 }
 
 export default ChartBar;
